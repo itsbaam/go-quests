3c36d3bf2aa901b91c2c2e894f621550d50edaf5
baamf18 <cnmferreira18@gmail.com>
2026-01-20 11:17:06 -0300

    refactor strings exercise solution

--- quests/009.strings/solution.go
+++ quests/009.strings/solution.go
@@ -1,5 +1,7 @@
 package strings
 
+import "unicode/utf8"
+
 type TextStats struct {
 	ByteLength int
 	RuneCount  int
@@ -8,36 +10,66 @@ type TextStats struct {
 func AnalyzeText(s string) TextStats {
 	return TextStats{
 		ByteLength: len(s),
-		RuneCount:  len([]rune(s)),
+		// RuneCount:  len([]rune(s)),
+		RuneCount: utf8.RuneCountInString(s),
 	}
 }
 
 func RuneFrequencies(s string) map[rune]int {
 	rF := make(map[rune]int)
-	for _, r := range []rune(s) {
+
+	// for _, r := range []rune(s) {
+	// 	rF[r] = rF[r] + 1
+	// }
+	for len(s) > 0 {
+		r, size := utf8.DecodeRuneInString(s)
 		rF[r] = rF[r] + 1
+		s = s[size:]
 	}
+
 	return rF
 }
 
 func FirstRunePosition(s string, target rune) int {
-	for i, r := range s {
+	// for i, r := range s {
+	// 	if r == target {
+	// 		return i
+	// 	}
+	// }
+	for i := 0; i < len(s); {
+		r, size := utf8.DecodeRuneInString(s[i:])
 		if r == target {
 			return i
 		}
+		i += size
 	}
 	return -1
 }
 
 func ExtractRunes(s string) []rune {
-	return []rune(s)
+	// return []rune(s)
+
+	runes := make([]rune, 0, utf8.RuneCountInString(s))
+	for len(s) > 0 {
+		r, size := utf8.DecodeRuneInString(s)
+		runes = append(runes, r)
+		s = s[size:]
+	}
+	return runes
 }
 
 func HasOnlyASCII(s string) bool {
-	for _, r := range []rune(s) {
+	// for _, r := range []rune(s) {
+	// 	if r > 127 {
+	// 		return false
+	// 	}
+	// }
+	for len(s) > 0 {
+		r, size := utf8.DecodeRuneInString(s)
 		if r > 127 {
 			return false
 		}
+		s = s[size:]
 	}
 	return true
 }
