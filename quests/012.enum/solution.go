0f614e12c8c541c972f5f3fdfae07398ad729550
baamf18 <cnmferreira18@gmail.com>
2026-01-24 17:41:38 -0300

    solve: 12 - enums

--- quests/012.enum/solution.go
+++ quests/012.enum/solution.go
@@ -12,12 +12,62 @@ const (
 	StateReturned
 )
 
+var stateStatus = map[OrderState]string{
+	StateCreated:   "created",
+	StatePaid:      "paid",
+	StatePacked:    "packed",
+	StateShipped:   "shipped",
+	StateDelivered: "delivered",
+	StateCancelled: "cancelled",
+	StateReturned:  "returned",
+}
+
 func (s OrderState) String() string {
-	// TODO: implement
-	return ""
+	return stateStatus[s]
 }
 
 func NextState(current OrderState, action string) OrderState {
-	// TODO: implement
+	if _, ok := stateStatus[current]; !ok {
+		panic("unknow state")
+	}
+
+	if current == StateCancelled || current == StateReturned {
+		return current
+	}
+
+	switch current {
+	case StateCreated:
+		switch action {
+		case "pay":
+			return StatePaid
+		case "cancel":
+			return StateCancelled
+		}
+	case StatePaid:
+		switch action {
+		case "pack":
+			return StatePacked
+		case "cancel":
+			return StateCancelled
+		}
+	case StatePacked:
+		switch action {
+		case "ship":
+			return StateShipped
+		}
+	case StateShipped:
+		switch action {
+		case "deliver":
+			return StateDelivered
+		case "return":
+			return StateReturned
+		}
+	case StateDelivered:
+		switch action {
+		case "return":
+			return StateReturned
+		}
+	}
+
 	return current
 }
