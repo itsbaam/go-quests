ce76a8a69ef7833ddf41421458ac0a4fde1b014c
baamf18 <cnmferreira18@gmail.com>
2026-01-25 15:32:36 -0300

    solve generics

--- quests/013.generics/solution.go
+++ quests/013.generics/solution.go
@@ -11,18 +11,47 @@ type element[T any] struct {
 
 // Push adds a value to the end of the list.
 func (lst *List[T]) Push(v T) {
-	// TODO: implement
+	newElement := &element[T]{val: v}
+
+	if lst.head == nil {
+		lst.head = newElement
+		lst.tail = newElement
+	} else {
+		lst.tail.next = newElement
+		lst.tail = newElement
+	}
 }
 
 // Pop removes the last element and returns it.
 func (lst *List[T]) Pop() (T, bool) {
-	// TODO: implement
 	var zero T
-	return zero, false
+
+	if lst.head == nil {
+		return zero, false
+	}
+
+	if lst.head == lst.tail {
+		val := lst.head.val
+		lst.head = nil
+		lst.tail = nil
+		return val, true
+	}
+
+	current := lst.head
+	for current.next != lst.tail {
+		current = current.next
+	}
+	val := lst.tail.val
+	current.next = nil
+	lst.tail = current
+	return val, true
 }
 
 // AllElements returns a slice of all elements in the list.
 func (lst *List[T]) AllElements() []T {
-	// TODO: implement
-	return nil
+	var result []T
+	for current := lst.head; current != nil; current = current.next {
+		result = append(result, current.val)
+	}
+	return result
 }
