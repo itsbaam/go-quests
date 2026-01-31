5e9b2ef0c2e754397a409144efe7783510de2e32
baamf18 <cnmferreira18@gmail.com>
2026-01-31 09:54:31 -0300

    solve errors

--- quests/014.error/solution.go
+++ quests/014.error/solution.go
@@ -1,9 +1,15 @@
 package error
 
+import (
+	"errors"
+	"fmt"
+	"strings"
+)
+
 // Part 1: Define sentinel errors
 var (
-// TODO: Define ErrEmptyFilename
-// TODO: Define ErrFileTooLarge
+	ErrEmptyFilename = errors.New("filename cannot be empty")
+	ErrFileTooLarge  = errors.New("file size exceeds limit")
 )
 
 // Part 2: Define custom error type
@@ -14,34 +20,55 @@ type ValidationError struct {
 
 // TODO: Implement Error() method
 func (ve *ValidationError) Error() string {
-	return ""
+	return fmt.Sprintf("validation failed for '%s': %s", ve.Filename, ve.Reason)
 }
 
 // Part 3: Validation functions
 
 func ValidateFilename(filename string) error {
-	// TODO: Implement
+	if filename == "" {
+		return ErrEmptyFilename
+	}
 	return nil
 }
 
 func ValidateFileSize(size int64, maxSize int64) error {
-	// TODO: Implement
+	if size > maxSize {
+		return ErrFileTooLarge
+	} else if size < 0 {
+		return errors.New("file size cannot be negative")
+	}
 	return nil
 }
 
 func ValidateFileExtension(filename string, allowedExts []string) error {
-	// TODO: Implement
-	return nil
+	for _, ext := range allowedExts {
+		if strings.HasSuffix(filename, ext) {
+			return nil
+		}
+	}
+	return &ValidationError{
+		Filename: filename,
+		Reason:   "unsupported file extension",
+	}
 }
 
 func ValidateFile(filename string, size int64, maxSize int64) error {
-	// TODO: Implement
+	if errName := ValidateFilename(filename); errName != nil {
+		return fmt.Errorf("file validation failed: %w", errName)
+	}
+	if errSize := ValidateFileSize(size, maxSize); errSize != nil {
+		return fmt.Errorf("file validation failed: %w", errSize)
+	}
 	return nil
 }
 
 // Part 4: Error checking
 
 func CanRetry(err error) bool {
-	// TODO: Implement
-	return false
+	if err == nil {
+		return false
+	}
+	var ve *ValidationError
+	return errors.As(err, &ve)
 }
